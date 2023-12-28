4360e3a67646ccc3b56e3ba38ca4f91b759806df
hoel45 <MrPvP2.minecraft@gmail.com>
2023-12-28 14:04:26 +0100

    Update pick_up.gd

--- Scene/PickUp/pick_up.gd
+++ Scene/PickUp/pick_up.gd
@@ -1,22 +1,16 @@
 extends Area2D
 
 var spriteId: int
-var names = ["Wood","Leather","Fabric","Coal","Iron","Gold","Amethyst","Emerald","Diamond"]
+var names_mat = ["Wood","Leather","Fabric","Coal","Iron","Gold","Amethyst","Emerald","Diamond"]
+var names_item = [[[]]]
 var itemLevel: int 
 var itemQuality: int
-var isCarry: bool = false
 
 func _ready():
 	itemQuality = (randi()% 3) +1
 	spriteId = randi() % 8
 	itemLevel = spriteId % 3 + 1
+	name = names_mat[spriteId]
+	#$AnimatedSprite2D.frame = spriteId
 	#$Name.text = name
 	#$quality.text = str(itemLevel) + "   " + str(itemQuality)
-	
-
-func _process(delta):
-	if isCarry :
-		#top_level = true
-		isCarry = false
-		pass
-
