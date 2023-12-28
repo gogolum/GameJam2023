ccc6ff7a199c7946a6ee1571251cc00dcf419cb9
gogolum <74010058+gogolum@users.noreply.github.com>
2023-12-28 13:56:37 +0100

    adding item arts

--- Scene/PickUp/pick_up.gd
+++ Scene/PickUp/pick_up.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 var spriteId: int
-var names = ["Wood","Leather","Fabric","Coal","Iron","Gold","Amethyst","Emerald","Diamond"]
+var names_mat = ["Wood","Leather","Fabric","Coal","Iron","Gold","Amethyst","Emerald","Diamond"]
+var names_item = [[[]]]
 var itemLevel: int 
 var itemQuality: int
 
@@ -9,7 +10,7 @@ func _ready():
 	itemQuality = (randi()% 3) +1
 	spriteId = randi() % 8
 	itemLevel = spriteId % 3 + 1
-	name = names[spriteId]
+	name = names_mat[spriteId]
 	#$AnimatedSprite2D.frame = spriteId
 	#$Name.text = name
 	#$quality.text = str(itemLevel) + "   " + str(itemQuality)
