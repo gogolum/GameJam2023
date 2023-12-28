d0a41cc1c1427109883d912ffdd981ddc7f4c531
gogolum <74010058+gogolum@users.noreply.github.com>
2023-12-28 14:25:14 +0100

    creating new scripts

--- Scene/PickUp/pick_up.gd
+++ Scene/PickUp/pick_up.gd
@@ -5,12 +5,20 @@ var names_mat = ["Wood","Leather","Fabric","Coal","Iron","Gold","Amethyst","Emer
 var names_item = [[[]]]
 var itemLevel: int 
 var itemQuality: int
+var itemType : int
 
 func _ready():
 	itemQuality = (randi()% 3) +1
 	spriteId = randi() % 8
 	itemLevel = spriteId % 3 + 1
 	name = names_mat[spriteId]
-	#$AnimatedSprite2D.frame = spriteId
+	if spriteId < 3:
+		itemType = 1
+	elif spriteId > 2:
+		itemType = 2
+	else :
+		itemType = 3
+		
+	$AnimatedSprite2D.frame = spriteId
 	#$Name.text = name
 	#$quality.text = str(itemLevel) + "   " + str(itemQuality)
